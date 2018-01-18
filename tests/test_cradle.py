dee64cfd692b24ce0d1436d1293590a7cc7f07f6
decvalts <declan.valters@manchester.ac.uk>
2018-01-18 20:12:38 +0000

    Tests with mock

---
+++ tests/test_cradle.py
@@ -0,0 +1,17 @@
+# Tests for the Cradle class
+import unittest
+from unittest.mock import patch
+
+import cradle as cr
+
+class testCradle(unittest.TestCase):
+    
+   @patch('builtins.input', return_value='X')
+   def test_Match(self):
+      cr = Cradle()
+      char_to_match = "X"
+      self.assertEqual(cr.Match("X"), char_to_match) 
+
+if __name__=='__main__':
+    unittest.main()
+
